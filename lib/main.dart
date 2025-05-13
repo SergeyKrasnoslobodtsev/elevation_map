f41ea7e2050d30f2028ef612e001f97bed60e11c
Сергей Краснослободцев <72604889+SergeyKrasnoslobodtsev@users.noreply.github.com>
2025-05-13 23:08:13 +0300

    Add internet permissions to AndroidManifest and enhance logging in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:elevation_map/screens/home_screen.dart';
 import 'package:flutter/material.dart';
-
+import 'dart:developer' as developer;
 import 'service_locator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  setupDependencies();
+  developer.log('Начинаем инициализацию приложения');
 
-  runApp(HomeScreen());
+  try {
+    developer.log('Настройка зависимостей');
+    setupDependencies();
+    developer.log('Зависимости настроены успешно');
+
+    developer.log('Запуск приложения');
+    runApp(HomeScreen());
+  } catch (e) {
+    developer.log('Ошибка при запуске: $e', error: e);
+  }
 }
