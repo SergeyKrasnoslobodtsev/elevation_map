b3b540f804ba3cf08044e117dbafc9f28ed37425
Сергей Краснослободцев <72604889+SergeyKrasnoslobodtsev@users.noreply.github.com>
2025-05-15 20:28:46 +0300

    Добавлены зависимости для работы с dotenv и Mapbox, обновлена структура приложения с использованием нового класса App, улучшена инициализация карты.

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,37 @@
 import 'package:elevation_map/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 import 'dart:developer' as developer;
 import 'service_locator.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   developer.log('Начинаем инициализацию приложения');
 
   try {
     developer.log('Настройка зависимостей');
+    await dotenv.load();
+    String token = dotenv.get('ACCESS_TOKEN');
+    MapboxOptions.setAccessToken(token);
     setupDependencies();
     developer.log('Зависимости настроены успешно');
 
     developer.log('Запуск приложения');
-    runApp(HomeScreen());
+    runApp(App());
   } catch (e) {
     developer.log('Ошибка при запуске: $e', error: e);
+  }
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.green),
+      home: HomeScreen(),
+    );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,90 +1,78 @@
-import 'package:elevation_map/widgets/map_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
-import 'package:provider/provider.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
-import '../viewmodels/home_view_model.dart';
-import '../widgets/elevation_chart_panel.dart';
-import '../widgets/layer_selector_panel.dart';
-import '../widgets/map_control_button.dart';
-
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  MapboxMap? mapboxMap;
+  var isLight = true;
+  final List<Object> hypsometricRamp = [
+    'interpolate', ['linear'], ['raster-value'],
+    //   высота (м)      ―――― RGBA ――――
+    0, ['rgba', 165, 0, 38, 1], // тёмно-красный
+    500, ['rgba', 215, 48, 39, 1], // красный
+    1000, ['rgba', 244, 109, 67, 1], // оранжево-красный
+    1500, ['rgba', 253, 174, 97, 1], // оранжевый
+    2000, ['rgba', 254, 224, 139, 1], // светло-жёлтый
+    3000, ['rgba', 217, 239, 139, 1], // жёлто-зелёный
+    4000, ['rgba', 166, 217, 106, 1], // зелёный
+    4500, ['rgba', 102, 189, 99, 1], // тёмно-зелёный
+  ];
+  _onMapCreated(MapboxMap mapboxMap) async {
+    this.mapboxMap = mapboxMap;
+    mapboxMap.scaleBar.updateSettings(ScaleBarSettings(isMetricUnits: true));
+    mapboxMap.logo.updateSettings(LogoSettings(enabled: false));
+    mapboxMap.attribution.updateSettings(AttributionSettings(enabled: false));
+  }
+
+  _onStyleLoadedCallback(StyleLoadedEventData data) async {
+    await mapboxMap!.style.addSource(
+      RasterDemSource(
+        id: 'terrain-dem',
+        url: 'mapbox://mapbox.mapbox-terrain-dem-v1',
+        tileSize: 512,
+      ),
+    );
+
+    // await mapboxMap!.style.addLayer(
+    //   RasterLayer(id: 'hypsometry', sourceId: 'terrain-dem')
+    //     // В Flutter-SDK 2.8.0 поле называется exactly **rasterColorExpression**
+    //     ..rasterColorExpression = hypsometricRamp
+    //     ..rasterOpacity = 1.0,
+    // );
+
+    await mapboxMap!.style.addLayer(
+      HillshadeLayer(id: 'terrain-hillshade', sourceId: 'terrain-dem')
+        ..hillshadeShadowColor =
+            Colors.red
+                .toARGB32() // тени
+        ..hillshadeHighlightColor =
+            Colors.amber
+                .toARGB32() // свет
+        ..hillshadeAccentColor =
+            Colors.deepOrange
+                .toARGB32() // грани
+        ..hillshadeExaggeration = 1.0,
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => GetIt.instance<HomeViewModel>(),
-      child: Consumer<HomeViewModel>(
-        builder: (context, viewModel, _) {
-          return MaterialApp(
-            home: Scaffold(
-              body: Stack(
-                children: [
-                  // Карта
-                  viewModel.isLoading
-                      ? const Center(child: CircularProgressIndicator())
-                      : MapWidget(
-                        currentLayer: viewModel.currentLayer,
-                        isElevationEnabled: viewModel.isElevationEnabled,
-                        isStreetAndRoadsEnabled:
-                            viewModel.isStreetAndRoadsEnabled,
-                        cacheStore: viewModel.cacheStore!,
-                        points: viewModel.routePoints,
-                        onMapTap: viewModel.onMapTap,
-                      ),
-
-                  if (viewModel.routeInfo != null &&
-                      viewModel.routePoints.length >= 2)
-                    Positioned(
-                      left: 0,
-                      right: 0,
-                      bottom: 0,
-                      child: ElevationChartPanel(
-                        routeInfo: viewModel.routeInfo!,
-                        onClose: viewModel.clearRoute,
-                      ),
-                    ),
-
-                  // Кнопки управления
-                  Positioned(
-                    top: 32,
-                    right: 16,
-                    child: MapControlButton(
-                      icon: Icons.layers,
-                      onTap: viewModel.toggleLayerSelector,
-                    ),
-                  ),
-                  // Positioned(
-                  //   top: 92,
-                  //   right: 16,
-                  //   child: MapControlButton(
-                  //     icon: Icons.layers,
-                  //     onTap: viewModel.toggleLayerSelector,
-                  //   ),
-                  // ),
-                  // Панель выбора слоев
-                  if (viewModel.showLayerSelector)
-                    Positioned(
-                      left: 0,
-                      right: 0,
-                      bottom: 0,
-                      child: LayerSelectorPanel(
-                        currentLayer: viewModel.currentLayer,
-                        isStreetAndRoadsEnabled:
-                            viewModel.isStreetAndRoadsEnabled,
-                        isElevationEnabled: viewModel.isElevationEnabled,
-                        onLayerSelected: viewModel.selectMapLayer,
-                        onStreetAndRoadsToggled: viewModel.toggleStreetAndRoads,
-                        onElevationToggled: viewModel.toggleElevation,
-                      ),
-                    ),
-                ],
-              ),
-            ),
-            debugShowCheckedModeBanner: false,
-          );
-        },
+    return Scaffold(
+      body: MapWidget(
+        key: ValueKey('mapWidget'),
+        cameraOptions: CameraOptions(
+          center: Point(coordinates: Position(37.618423, 55.751244)),
+          zoom: 10,
+        ),
+        styleUri: MapboxStyles.OUTDOORS,
+        onMapCreated: _onMapCreated,
+        onStyleLoadedListener: _onStyleLoadedCallback,
       ),
     );
   }
